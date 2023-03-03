7596f21706c8fabaa416b91ea65de1d512b725fb
hungdao9919 <hungdao9919@gmail.com>
2023-03-03 22:30:03 +0700

    Create Verify JWT middleware

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -2,8 +2,14 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const handleVerifyJWT = (req,res,next)=>{ 
      
-    const token = req?.headers?.authorization?.split(" ")[1] 
-    const decoded = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET) 
-    next();
+
+    const token = req?.headers?.authorization?.split(" ")[1]  
+    if(!token) return res.sendStatus(401)
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+        if(err) return res.sendStatus(403) 
+        req.username = decoded.userInfo.username
+        req.roles = decoded.userInfo.roles
+        next();
+    });
 }
 module.exports=handleVerifyJWT;
