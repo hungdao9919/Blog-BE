4c3e2909a443e29904fa0869c8abf5679e8ba9c2
hungdao9919 <hungdao9919@gmail.com>
2023-03-12 14:31:29 +0700

    log out controller

---
+++ controller/logOutController.js
@@ -0,0 +1,30 @@
+ 
+const user = require('../model/user');
+
+const handleLogOut = async (req,res)=>{
+    
+
+    const cookies = req.cookies
+    if(!cookies?.jwt) return res.sendStatus(204); //No content
+    const refreshToken = cookies.jwt;
+    
+    const foundUser = await user.findOne({refreshToken:refreshToken}).exec();
+
+    if(!foundUser) {
+        res.clearCookie('jwt',{httpOnly:true})
+        return res.sendStatus(403) //forbiden
+
+    }
+ 
+
+    foundUser.refreshToken=''
+    const result = await foundUser.save();
+    console.log(result)
+     
+    res.clearCookie('jwt',{httpOnly:true})
+    res.sendStatus(204)
+
+}
+        
+
+module.exports = handleLogOut;
