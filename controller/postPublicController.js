be624b4b770670b17f6480939a8fcda680209aaf
hungdao9919 <hungdao9919@gmail.com>
2023-03-27 00:24:01 +0700

    Update postPublicController

--- controller/postPublicController.js
+++ controller/postPublicController.js
@@ -1,9 +1,29 @@
 const post = require('../model/post');
+const user = require('../model/user');
  
 const handleGetPublicPosts = async (req, res) => {
     const allPost = await post.find({})
-   if(allPost.length===0) return res.status(204).json({"message":"Do not have any posts"})
-   res.status(200).json(allPost)
+    if(allPost.length===0) return res.status(204).json({"message":"Do not have any posts"})
+    
+    let postsResult=[]
+    for(var i=0;i < allPost.length;i++){
+        const foundUser = await user.findOne({'_id':allPost[i].userid});
+        let username
+        console.log(allPost[i])
+        if(!foundUser){
+            username = 'removed user'
+        }
+        else{
+            username = foundUser.username
+        }
+        postsResult.push({'title':allPost[i].title,'postcontent':allPost[i].postcontent,'datecreated':allPost[i].datecreated,'datemodify':allPost[i].datemodify,'username':username}) 
+    }
+    
+    return res.status(200).json(postsResult)
+        
+        
+        
+    
 
 }
 module.exports =  handleGetPublicPosts;
