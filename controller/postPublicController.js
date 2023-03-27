b2b19e1733d68c70c169500ab1412b8f10878d02
hungdao9919 <hungdao9919@gmail.com>
2023-03-27 15:00:05 +0700

    Fix postPublicContrller: sort the result

--- controller/postPublicController.js
+++ controller/postPublicController.js
@@ -2,10 +2,9 @@ const post = require('../model/post');
 const user = require('../model/user');
  
 const handleGetPublicPosts = async (req, res) => {
-    const allPost = await post.find({})
-    if(allPost.length===0) return res.status(204).json({"message":"Do not have any posts"})
-    
-    let postsResult=[]
+    const allPost = await post.find({})  
+    if(allPost.length===0) return res.status(204).json({"message":"Do not have any posts"}) 
+    let postsResult=[] 
     for(var i=0;i < allPost.length;i++){ 
 
         const foundUser = await user.findOne({'_id':allPost[i].userid});
@@ -16,9 +15,18 @@ const handleGetPublicPosts = async (req, res) => {
         else{
             username = foundUser.username
         }
-        postsResult.push({'id':allPost[i]._id,'title':allPost[i].title,'postcontent':allPost[i].postcontent,'datecreated':allPost[i].datecreated,'datemodify':allPost[i].datemodify,'username':username}) 
-    } 
-    return res.status(200).json(postsResult)
+        //fix lại format của ngày tháng năm
+        const newModifiDate = `${allPost[i].datemodify.split(" ")[1].split("/")[2]}/${allPost[i].datemodify.split(" ")[1].split("/")[1]}/${allPost[i].datemodify.split(" ")[1].split("/")[0]} ${allPost[i].datemodify.split(" ")[0]}`
+        postsResult.push({'id':allPost[i]._id,'title':allPost[i].title,'postcontent':allPost[i].postcontent,'datecreated':allPost[i].datecreated,'datemodify':newModifiDate,'username':username})  
+   
+          
+        
+    }  
+    const sortedResult =  postsResult.sort((a,b)=>{
+        return  Date.parse(b.datemodify) - Date.parse(a.datemodify)
+    })
+     
+    return res.status(200).json(sortedResult)
         
         
         
