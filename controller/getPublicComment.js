cf4a5d29ed543e061c7591cdbe7bc1f81ff57e0f
hungdao9919 <hungdao9919@gmail.com>
2023-03-29 16:46:02 +0700

    Sort and limit results

--- controller/getPublicComment.js
+++ controller/getPublicComment.js
@@ -4,7 +4,7 @@ const user = require('../model/user');
 const handleGetPublicComment = async (req, res) => {
     const  postid = req.params.id;
      
-    const allComments = await comment.find({"postid":postid})
+    const allComments = await comment.find({"postid":postid}).sort('-createdAt')
     if(allComments.length === 0) return res.status(204).json({"message":"Do not have any posts"})
     let commentResult=[] 
     for(var i=0;i < allComments.length;i++){  
@@ -19,18 +19,14 @@ const handleGetPublicComment = async (req, res) => {
             username = foundUser.username
             profileImage = foundUser.profileImage
         }
-        //fix lại format của ngày tháng năm
-        const newModifiDate = `${allComments[i].datemodify.split(" ")[1].split("/")[2]}/${allComments[i].datemodify.split(" ")[1].split("/")[1]}/${allComments[i].datemodify.split(" ")[1].split("/")[0]} ${allComments[i].datemodify.split(" ")[0]}`
-        commentResult.push({'id':allComments[i]._id,'commentcontent':allComments[i].commentcontent,'datecreated':allComments[i].datecreated,'datemodify':newModifiDate,'username':username, 'profileImage':profileImage,'userid':allComments[i].userid})  
-   
+        const newCmt = {...allComments[i].toObject(),'username':username, 'profileImage':profileImage} 
+        commentResult.push(newCmt)
           
         
     }  
 
-    const sortedResult =  commentResult.sort((a,b)=>{
-        return  Date.parse(b.datemodify) - Date.parse(a.datemodify)
-    }) 
-    res.status(200).json(sortedResult) 
+     
+    res.status(200).json(commentResult) 
 
 }
 module.exports =  handleGetPublicComment; 
--- controller/postPublicController.js
+++ controller/postPublicController.js
@@ -2,8 +2,8 @@ const post = require('../model/post');
 const user = require('../model/user');
  
 const handleGetPublicPosts = async (req, res) => {
-    const allPost = await post.find({})  
-    if(allPost.length===0) return res.status(204).json({"message":"Do not have any posts"}) 
+    const allPost = await post.find({}).limit(999).sort('-createdAt')
+    if(allPost.length===0) return res.status(204).json({"message":"Do not have any posts"})  
     let postsResult=[] 
     for(var i=0;i < allPost.length;i++){ 
 
@@ -15,18 +15,15 @@ const handleGetPublicPosts = async (req, res) => {
         else{ 
             username = foundUser.username
         }
-        //fix lại format của ngày tháng năm
-        const newModifiDate = `${allPost[i].datemodify.split(" ")[1].split("/")[2]}/${allPost[i].datemodify.split(" ")[1].split("/")[1]}/${allPost[i].datemodify.split(" ")[1].split("/")[0]} ${allPost[i].datemodify.split(" ")[0]}`
-        postsResult.push({'id':allPost[i]._id,'title':allPost[i].title,'postcontent':allPost[i].postcontent,'datecreated':allPost[i].datecreated,'datemodify':newModifiDate,'username':username, 'userid':allPost[i].userid})  
-   
+         
+            
+        const newPost = {...allPost[i].toObject(),'username':username} 
+        postsResult.push(newPost)
+         
           
         
-    }  
-    const sortedResult =  postsResult.sort((a,b)=>{
-        return  Date.parse(b.datemodify) - Date.parse(a.datemodify)
-    })
-     
-    return res.status(200).json(sortedResult)
+    }      
+    return res.status(200).json(postsResult)
         
         
         
